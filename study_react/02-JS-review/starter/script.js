287f66185e5b4e27ca709b81c666716ad02cbc49
Siddami <dharmmy621@gmail.com>
2025-04-27 17:05:48 +0100

    feat: add javascript review

--- study_react/02-JS-review/starter/script.js
+++ study_react/02-JS-review/starter/script.js
@@ -219,3 +219,17 @@ function getBook(id) {
 // const bookAfterUpdate = bookAfterDelete.map(book => book.id === 1? {}: book)
 // bookAfterUpdate
 
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then((res)=>res.json())
+// .then((data) => console.log(data))
+
+async function getTodos(){
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos)
